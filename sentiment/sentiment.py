1489c993dfa6fffef5143896d4fd93efff6366b8
Eduardo Pacheco <edupac@gmail.com>
2024-04-25 20:31:04 +0200

    new vm commit

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -10,10 +10,10 @@ def main():
     parser.add_argument('comment_text', help='column in the file that contains the text')
     args = parser.parse_args()
 
-    df_text = pd.read_csv(args.file)#, lineterminator='\n')
+    df_text = pd.read_csv(args.comments_cleaned)#, lineterminator='\n')
     # model = 'finiteautomata/bertweet-base-sentiment-analysis'
     model = 'cardiffnlp/twitter-xlm-roberta-base-sentiment'
-    sentiment = pipeline(task='sentiment-analysis', model=model)
+    sentiment = pipeline(task='sentiment-analysis', model=model, tokenizer=model)
     counter = 0
 
     # Define the total number of iterations
@@ -24,15 +24,15 @@ def main():
 
     for index, row in df_text.iterrows():
         try:
-            df_text.loc[index, 'sentiment'] = str(sentiment(str(row[args.text_column])[:279])[0]['label'])
-            df_text.loc[index, 'sentiment_score'] = float(sentiment(str(row[args.text_column])[:279])[0]['score'])
+            df_text.loc[index, 'sentiment'] = str(sentiment(str(row[args.comment_text])[:279])[0]['label'])
+            df_text.loc[index, 'sentiment_score'] = float(sentiment(str(row[args.comment_text])[:279])[0]['score'])
         except Exception as e:
-            print(f'{str(row[args.text_column])}', f'Error -> {e}')
+            print(f'{str(row[args.comment_text])}', f'Error -> {e}')
         counter += 1
         progress_bar.update(1)  # Update the progress bar
         progress_bar.set_description(f'Processed: {counter}/{total_iterations}')
 
-    df_text.to_csv(f'sentiment_analysis_{args.file}', index=False)
+    df_text.to_csv(f'sentiment_analysis_{args.comments_cleaned}', index=False)
 
 
 if __name__ == '__main__':
